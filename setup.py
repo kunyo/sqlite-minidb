844b90ece400a3fbeb1114956ab361b35e3235e3
kn <kntrashcan@gmail.com>
2020-09-27 12:25:59 +0000

    Amended version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='minidb',
-    version='1.0',
+    version='1.0.0',
     url='https://github.com/kunyo/sqlite-minidb',
     license='UNLICENSED',
     author='KN',
